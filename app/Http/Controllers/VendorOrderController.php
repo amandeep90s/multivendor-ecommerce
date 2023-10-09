c14e4ef0cfb8de967708a4a4139b5922279f3096
Amandeep Singh <programmeramandeep@gmail.com>
2023-10-09 22:15:59 +0530

    Vendor Registration and Management

---
+++ app/Http/Controllers/VendorOrderController.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\StoreVendorOrderRequest;
+use App\Http\Requests\UpdateVendorOrderRequest;
+use App\Models\VendorOrder;
+
+class VendorOrderController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(StoreVendorOrderRequest $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(VendorOrder $vendorOrder)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(VendorOrder $vendorOrder)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(UpdateVendorOrderRequest $request, VendorOrder $vendorOrder)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(VendorOrder $vendorOrder)
+    {
+        //
+    }
+}
---
+++ app/Http/Controllers/VendorReviewController.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\StoreVendorReviewRequest;
+use App\Http\Requests\UpdateVendorReviewRequest;
+use App\Models\VendorReview;
+
+class VendorReviewController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(StoreVendorReviewRequest $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(VendorReview $vendorReview)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(VendorReview $vendorReview)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(UpdateVendorReviewRequest $request, VendorReview $vendorReview)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(VendorReview $vendorReview)
+    {
+        //
+    }
+}
