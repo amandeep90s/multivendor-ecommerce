02b491d0f1441ddd49fc41d7cfd4fedb731322a3
Amandeep Singh <programmeramandeep@gmail.com>
2023-10-09 22:41:33 +0530

    Legal Considerations

---
+++ app/Http/Controllers/TermAndConditionController.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\StoreTermAndConditionRequest;
+use App\Http\Requests\UpdateTermAndConditionRequest;
+use App\Models\TermAndCondition;
+
+class TermAndConditionController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(StoreTermAndConditionRequest $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(TermAndCondition $termAndCondition)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(TermAndCondition $termAndCondition)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(UpdateTermAndConditionRequest $request, TermAndCondition $termAndCondition)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(TermAndCondition $termAndCondition)
+    {
+        //
+    }
+}
